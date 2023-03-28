1738b2b8668c1b4337b4a3ff0b7f259e8660728b
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-03-28 07:49:46 +0700

    edit error message use raise HTTPEXception

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status, Response
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models, database
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -31,7 +31,9 @@ def all(db : Session = Depends(get_db)):
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {'default': f"Blog with the id {id} is not available"}
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with the id {id} is not available')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'default': f"Blog with the id {id} is not available"}
     
     return blog
