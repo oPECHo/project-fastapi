1b7911b397b80c87e4af4f48d55f13e1e6e5fb81
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-03-28 08:48:24 +0700

    create method update(put) and edit method delete

--- blog/main.py
+++ blog/main.py
@@ -24,11 +24,24 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
 
 @app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
 def destory(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
-
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    return 'updated'
 
 @app.get('/blog')
 def all(db : Session = Depends(get_db)):
