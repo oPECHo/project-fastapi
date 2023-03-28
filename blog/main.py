3a299616def4a224beda6fe10bf5113d452d5512
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-03-28 08:02:54 +0700

    create method delete

--- blog/main.py
+++ blog/main.py
@@ -22,6 +22,14 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destory(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+
 @app.get('/blog')
 def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
