9cfdf772ef9dcb21ee84853a5553b173745d59f2
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-03-29 15:13:29 +0700

    Create file blog in folder repository

---
+++ blog/repository/blog.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+from .. import models, schemas
+from fastapi import HTTPException, status
+
+def get_all(db: Session):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+def create(request,db: Session):
+    new_blog = models.Blog(title=request.title, body=request.body,user_id = 1)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+def destory(id:int, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+def update(id:int, request:schemas, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    return 'updated'
+
+def show(id:int, db: Session):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with the id {id} is not available')
+    return blog
