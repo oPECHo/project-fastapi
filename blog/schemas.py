d43c74f50e3a0d4130dd6322114e15fb0d9e36c2
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-03-29 13:42:14 +0700

    edit class order

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,16 +1,14 @@
+from typing import List
 from pydantic import BaseModel
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
-class ShowBlog(BaseModel):
-    title : str
-    body: str
+class Blog(BlogBase):
     class Config():
         orm_mode = True
-
 
 class User(BaseModel):
     username:str
@@ -20,6 +18,14 @@ class User(BaseModel):
 class ShowUser(BaseModel):
     username:str
     email:str
-    
+    blogs : List[Blog] = []
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title : str
+    body: str
+    creator: ShowUser
+
     class Config():
         orm_mode = True
