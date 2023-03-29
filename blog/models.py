30e12e93fba644c1ace6aef149143d6d0339b0f3
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-03-29 13:40:18 +0700

    Create relationship

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from .database import Base
-
+from sqlalchemy.orm import relationship
 
 class Blog(Base):
     __tablename__ = 'blogs'
@@ -8,6 +8,9 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title =Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    creator = relationship("User", back_populates="blogs")
 
 class User(Base):
     __tablename__ = 'users'
@@ -16,3 +19,5 @@ class User(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+
+    blogs = relationship("Blog", back_populates="creator")
